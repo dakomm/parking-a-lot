a333810964dfe9452828a931b0863433a026fcda
DAKomm <dalove1583@gmail.com>
2021-06-26 17:42:05 +0900

    AppBar+Drawer 추가

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, useEffect, useState } from "react";
+import ClippedAppBar from "./components/ClippedAppBar";
+// import Main from "./components/main";
 
-function App() {
+import { withStyles } from "@material-ui/core";
+
+const styles = theme => ({
+  root:{
+    width: "100%",
+  },
+});
+
+export default function App() {
+  //const classes = props;
+  const classes = styles();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ClippedAppBar/>
+      {/* <Main/> */}
+      
     </div>
   );
 }
 
-export default App;
+//export default withStyles(styles)(App);
